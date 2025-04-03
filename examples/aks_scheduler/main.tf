d03da264ba50cb1d081ec7087ef869e2e6d50dc2
diodonfrost <diodon.frost@diodonfrost.me>
2025-04-03 21:35:21 +0000

    tests(aks): use correct instance size
    Update AKS example clusters to use the minimum VM sizes,
    replacing the budget-tier Standard_B1s and Standard_B2s
    instances with the more performant Standard_D2s_v5.

--- examples/aks_scheduler/main.tf
+++ examples/aks_scheduler/main.tf
@@ -34,7 +34,7 @@ resource "azurerm_kubernetes_cluster" "to_stop" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_B1s"
+    vm_size    = "Standard_D2s_v5"
   }
 
   identity {
@@ -57,7 +57,7 @@ resource "azurerm_kubernetes_cluster" "do_no_not" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_B2s"
+    vm_size    = "Standard_D2s_v5"
   }
 
   identity {
