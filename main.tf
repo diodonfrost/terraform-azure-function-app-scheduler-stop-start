a508955c63aba9e344e344740d35f0c66f081283
diodonfrost <diodon.frost@diodonfrost.me>
2025-04-04 21:17:12 +0000

    feat(terraform): upgrade function app python runtime from 3.9 to 3.11
    Update the Azure Function App's Python runtime
    to version 3.11 from the previous 3.9 version.

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "azurerm_linux_function_app" "this" {
     application_insights_connection_string = azurerm_application_insights.this.connection_string
     application_insights_key               = azurerm_application_insights.this.instrumentation_key
     application_stack {
-      python_version = "3.9"
+      python_version = "3.11"
     }
     cors {
       allowed_origins = ["https://portal.azure.com"]
