a5f8de717aa5bb07d11689336687bc7736269b16
diodonfrost <diodon.frost@diodonfrost.me>
2025-04-07 00:09:19 +0000

    refactor(container_handler): improve scheduler code organization
    Implement _perform_action to centralize start/stop operations
    to reduce code duplication between start and stop methods.

--- functions/scheduler/container_group_handler.py
+++ functions/scheduler/container_group_handler.py
@@ -1,6 +1,7 @@
 """Container group handler module."""
 
 import logging
+from typing import Callable, Tuple
 
 from azure.core.exceptions import AzureError
 from azure.identity import DefaultAzureCredential
@@ -13,6 +14,8 @@ from .filter_resources_by_tags import FilterByTags
 class ContainerGroupScheduler:
     """Abstract container group scheduler in a class."""
 
+    RESOURCE_TYPE = "Microsoft.ContainerInstance/containerGroups"
+
     def __init__(self, subscription_id: str) -> None:
         """Initialize Azure client."""
         self.container_client = ContainerInstanceManagementClient(
@@ -20,42 +23,59 @@ class ContainerGroupScheduler:
         )
         self.tag_filter = FilterByTags(subscription_id)
 
+    def _parse_resource_id(self, resource_id: str) -> Tuple[str, str]:
+        """Extract resource group name and container group name from resource ID.
+
+        :param str resource_id: The Azure resource ID
+        :return: Tuple of (resource_group_name, container_group_name)
+        """
+        parts = resource_id.split("/")
+        return parts[4], parts[-1]
+
+    def _perform_action(
+        self, azure_tags: dict, action: str, operation: Callable
+    ) -> None:
+        """Execute an operation on container groups matching the tags.
+
+        :param dict azure_tags: Tags to filter resources by
+        :param str action: Action name for logging ("Start" or "Stop")
+        :param Callable operation: Function to call on each resource
+        """
+        for container_id in self.tag_filter.get_resources(
+            azure_tags, self.RESOURCE_TYPE
+        ):
+            try:
+                resource_group, container_name = self._parse_resource_id(container_id)
+                operation(
+                    resource_group_name=resource_group,
+                    container_group_name=container_name,
+                )
+                logging.info("%s Container group: %s", action, container_id)
+            except AzureError as exc:
+                azure_exceptions("container_group", container_id, exc)
+
     def stop(self, azure_tags: dict) -> None:
         """Azure Container group stop function.
 
         Stop container group with the defined tags.
 
-        :param str azure_tags:
+        :param dict azure_tags:
             The key of the tag that you want to filter by.
             For example: {"key": "value"}
         """
-        resource_type = "Microsoft.ContainerInstance/containerGroups"
-        for container_id in self.tag_filter.get_resources(azure_tags, resource_type):
-            try:
-                self.container_client.container_groups.stop(
-                    resource_group_name=container_id.split("/")[4],
-                    container_group_name=container_id.split("/")[-1],
-                )
-                logging.info("Stop Container group: %s", container_id)
-            except AzureError as exc:
-                azure_exceptions("container_group", container_id, exc)
+        self._perform_action(
+            azure_tags, "Stop", self.container_client.container_groups.stop
+        )
 
     def start(self, azure_tags: dict) -> None:
         """Azure Container group start function.
 
         Start container group with the defined tags.
 
-        :param str azure_tags:
+        :param dict azure_tags:
             The key of the tag that you want to filter by.
             For example: {"key": "value"}
         """
-        resource_type = "Microsoft.ContainerInstance/containerGroups"
-        for container_id in self.tag_filter.get_resources(azure_tags, resource_type):
-            try:
-                self.container_client.container_groups.begin_start(
-                    resource_group_name=container_id.split("/")[4],
-                    container_group_name=container_id.split("/")[-1],
-                )
-                logging.info("Start Container group: %s", container_id)
-            except AzureError as exc:
-                azure_exceptions("container_group", container_id, exc)
+        self._perform_action(
+            azure_tags, "Start", self.container_client.container_groups.begin_start
+        )
