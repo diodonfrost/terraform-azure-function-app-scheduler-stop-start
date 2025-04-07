6e9fb38cf82701507d81669aaf4030aed02453d3
diodonfrost <diodon.frost@diodonfrost.me>
2025-04-07 00:10:01 +0000

    refactor(aks_handler): improve scheduler code organization
    Implement _perform_action to centralize start/stop operations
    to reduce code duplication between start and stop methods.

--- functions/scheduler/aks_handler.py
+++ functions/scheduler/aks_handler.py
@@ -1,6 +1,7 @@
 """Aks cluster handler module."""
 
 import logging
+from typing import Callable, Tuple
 
 from azure.core.exceptions import AzureError
 from azure.identity import DefaultAzureCredential
@@ -13,6 +14,8 @@ from .filter_resources_by_tags import FilterByTags
 class AksScheduler:
     """Abstract aks scheduler in a class."""
 
+    RESOURCE_TYPE = "Microsoft.ContainerService/managedClusters"
+
     def __init__(self, subscription_id: str) -> None:
         """Initialize Azure client."""
         self.aks_client = ContainerServiceClient(
@@ -20,42 +23,56 @@ class AksScheduler:
         )
         self.tag_filter = FilterByTags(subscription_id)
 
+    def _parse_resource_id(self, resource_id: str) -> Tuple[str, str]:
+        """Extract resource group name and AKS cluster name from resource ID.
+
+        :param str resource_id: The Azure resource ID
+        :return: Tuple of (resource_group_name, resource_name)
+        """
+        parts = resource_id.split("/")
+        return parts[4], parts[-1]
+
+    def _perform_action(
+        self, azure_tags: dict, action: str, operation: Callable
+    ) -> None:
+        """Execute an operation on AKS clusters matching the tags.
+
+        :param dict azure_tags: Tags to filter resources by
+        :param str action: Action name for logging ("Start" or "Stop")
+        :param Callable operation: Function to call on each resource
+        """
+        for aks_id in self.tag_filter.get_resources(azure_tags, self.RESOURCE_TYPE):
+            try:
+                resource_group, resource_name = self._parse_resource_id(aks_id)
+                operation(
+                    resource_group_name=resource_group, resource_name=resource_name
+                )
+                logging.info("%s aks cluster: %s", action, aks_id)
+            except AzureError as exc:
+                azure_exceptions("aks", aks_id, exc)
+
     def stop(self, azure_tags: dict) -> None:
         """Azure aks cluster stop function.
 
         Stop aks with defined tags.
 
-        :param str azure_tags:
+        :param dict azure_tags:
             The key of the tag that you want to filter by.
             For example: {"key": "value"}
         """
-        resource_type = "Microsoft.ContainerService/managedClusters"
-        for aks_id in self.tag_filter.get_resources(azure_tags, resource_type):
-            try:
-                self.aks_client.managed_clusters.begin_stop(
-                    resource_group_name=aks_id.split("/")[4],
-                    resource_name=aks_id.split("/")[-1],
-                )
-                logging.info("Stop aks cluster: %s", aks_id)
-            except AzureError as exc:
-                azure_exceptions("aks", aks_id, exc)
+        self._perform_action(
+            azure_tags, "Stop", self.aks_client.managed_clusters.begin_stop
+        )
 
     def start(self, azure_tags: dict) -> None:
         """Azure aks cluster start function.
 
         Start aks with defined tags.
 
-        :param str azure_tags:
+        :param dict azure_tags:
             The key of the tag that you want to filter by.
             For example: {"key": "value"}
         """
-        resource_type = "Microsoft.ContainerService/managedClusters"
-        for aks_id in self.tag_filter.get_resources(azure_tags, resource_type):
-            try:
-                self.aks_client.managed_clusters.begin_start(
-                    resource_group_name=aks_id.split("/")[4],
-                    resource_name=aks_id.split("/")[-1],
-                )
-                logging.info("Start aks cluster: %s", aks_id)
-            except AzureError as exc:
-                azure_exceptions("aks", aks_id, exc)
+        self._perform_action(
+            azure_tags, "Start", self.aks_client.managed_clusters.begin_start
+        )
