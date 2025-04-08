bd70dc6fb33388c9ce5bbd1aebc8c3c686e65e3c
diodonfrost <diodon.frost@diodonfrost.me>
2025-04-08 18:58:09 +0000

    tests(examples): add spot instances example for vm scheduler
    Add a complete example demonstrating how to use the module
    with Azure spot instances. The example includes:
    * Configuration for spot VM with priority, eviction policy, and max bid price
    * Scheduled start/stop functions with cron expressions
    * Test configuration to validate infrastructure setup
    * Documentation for running the example

---
+++ examples/spot_instances/main.tf
@@ -0,0 +1,111 @@
+resource "random_pet" "suffix" {}
+
+resource "random_id" "suffix" {
+  byte_length = 6
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "test-${random_pet.suffix.id}"
+  location = "swedencentral"
+}
+
+resource "azurerm_virtual_network" "test" {
+  name                = "test-${random_pet.suffix.id}"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+}
+
+resource "azurerm_subnet" "test" {
+  name                 = "test-${random_pet.suffix.id}"
+  resource_group_name  = azurerm_resource_group.test.name
+  virtual_network_name = azurerm_virtual_network.test.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "to_stop" {
+  name                = "test-to-stop-${random_pet.suffix.id}"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.test.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "to_stop" {
+  name                = "test-to-stop-${random_pet.suffix.id}"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+  size                = "Standard_D2s_v4"
+  priority            = "Spot"
+  eviction_policy     = "Deallocate"
+  max_bid_price       = -1
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.to_stop.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = tls_private_key.test.public_key_openssh
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+
+  tags = {
+    tostop = "true"
+  }
+}
+
+resource "tls_private_key" "test" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+
+module "stop_virtual_machines" {
+  source = "../../"
+
+  resource_group_name           = azurerm_resource_group.test.name
+  location                      = azurerm_resource_group.test.location
+  function_app_name             = "fpn-to-stop-${random_pet.suffix.id}"
+  service_plan_name             = "spn-to-stop-${random_pet.suffix.id}"
+  storage_account_name          = "santostop${random_id.suffix.hex}"
+  enable_application_insights   = true
+  scheduler_action              = "stop"
+  scheduler_ncrontab_expression = "0 22 * * *"
+  virtual_machine_schedule      = "true"
+  scheduler_tag = {
+    tostop = "true"
+  }
+}
+
+module "start_virtual_machines" {
+  source = "../../"
+
+  resource_group_name           = azurerm_resource_group.test.name
+  location                      = azurerm_resource_group.test.location
+  function_app_name             = "fpn-to-start-${random_pet.suffix.id}"
+  service_plan_name             = "spn-to-start-${random_pet.suffix.id}"
+  storage_account_name          = "santostart${random_id.suffix.hex}"
+  enable_application_insights   = true
+  scheduler_action              = "start"
+  scheduler_ncrontab_expression = "0 7 * * *"
+  virtual_machine_schedule      = "true"
+  scheduler_tag = {
+    tostop = "true"
+  }
+}
---
+++ examples/spot_instances/terraform.tftest.hcl
@@ -0,0 +1,69 @@
+run "create_test_infrastructure" {
+  command = apply
+
+  assert {
+    condition     = module.stop_virtual_machines.resource_group_name == azurerm_resource_group.test.name
+    error_message = "Invalid resource group name"
+  }
+
+  assert {
+    condition     = module.stop_virtual_machines.resource_group_name == azurerm_resource_group.test.name
+    error_message = "Invalid resource group name"
+  }
+
+  assert {
+    condition     = module.stop_virtual_machines.function_app_name == "fpn-to-stop-${random_pet.suffix.id}"
+    error_message = "Invalid function app name"
+  }
+
+  assert {
+    condition     = module.start_virtual_machines.service_plan_name == "spn-to-start-${random_pet.suffix.id}"
+    error_message = "Invalid service plan name"
+  }
+
+  assert {
+    condition     = module.start_virtual_machines.function_app_name == "fpn-to-start-${random_pet.suffix.id}"
+    error_message = "Invalid function app name"
+  }
+}
+
+#run "verify_function_app_execution" {
+#  command = plan
+#
+#  module {
+#    source = "./test-execution"
+#
+#    resource_group_name = module.stop_virtual_machines.resource_group_name
+#    function_app_name   = module.stop_virtual_machines.function_app_name
+#    vm_1_to_stop        = azurerm_linux_virtual_machine[0].to_stop.name
+#    vm_2_to_stop        = azurerm_linux_virtual_machine[1].to_stop.name
+#    vm_3_to_stop        = azurerm_linux_virtual_machine[2].to_stop.name
+#    vm_1_do_not_stop    = azurerm_linux_virtual_machine[0].do_not_stop.name
+#    vm_2_do_not_stop    = azurerm_linux_virtual_machine[1].do_not_stop.name
+#  }
+#
+#  assert {
+#    condition     = output.vm_1_to_stop.power_state == "Deallocated"
+#    error_message = "Virtual machine 1 to stop is not deallocated"
+#  }
+#
+#  assert {
+#    condition     = output.vm_2_to_stop.power_state == "Deallocated"
+#    error_message = "Virtual machine 2 to stop is not deallocated"
+#  }
+#
+#  assert {
+#    condition     = output.vm_3_to_stop.power_state == "Deallocated"
+#    error_message = "Virtual machine 3 to stop is not deallocated"
+#  }
+#
+#  assert {
+#    condition     = output.vm_1_to_stop.power_state == "Running"
+#    error_message = "Virtual machine 1 to stop is not Running"
+#  }
+#
+#  assert {
+#    condition     = output.vm_2_to_stop.power_state == "Running"
+#    error_message = "Virtual machine 2 to stop is not Running"
+#  }
+#}
