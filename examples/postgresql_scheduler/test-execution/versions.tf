c163c7af27d1ffd77eea0eae1005c8ffb16cea3f
diodonfrost <diodon.frost@diodonfrost.me>
2025-04-14 18:37:44 +0000

    tests(examples): apply terraform validate linters
    Add versions.tf files in all test-execution directories
    to define consistent Terraform provider requierements
    accros test environments. The files specify:
    * Terraform version ≥ 1.3.0
    * Azure provider 3.x
    * Null provider 3.x
    * Local provider 2.x
    * Time provider 0.13.0

--- examples/postgresql_scheduler/test-execution/versions.tf
+++ examples/postgresql_scheduler/test-execution/versions.tf
@@ -5,17 +5,17 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.0.0, < 4.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.0.0, < 4.0"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = ">= 4.0.0"
-    }
     null = {
       source  = "hashicorp/null"
       version = ">= 3.0.0, < 4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.0.0, < 3.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.13.0"
     }
   }
 }
