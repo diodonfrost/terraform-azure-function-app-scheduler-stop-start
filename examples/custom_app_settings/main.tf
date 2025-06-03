52466be465e396746f678d4a03854477f2a9d984
diodonfrost <diodon.frost@diodonfrost.me>
2025-06-03 21:37:36 +0000

    tests(custom_app_settings): update storage account naming
    This commit modifies the custom app settings example
    to generate unique storage account names for both
    stopping and starting virtual machines.

--- examples/custom_app_settings/main.tf
+++ examples/custom_app_settings/main.tf
@@ -9,26 +9,17 @@ resource "azurerm_resource_group" "test" {
   location = "swedencentral"
 }
 
-resource "azurerm_storage_account" "test" {
-  name                     = "test${random_id.suffix.hex}"
-  resource_group_name      = azurerm_resource_group.test.name
-  location                 = azurerm_resource_group.test.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
 module "stop_virtual_machines" {
   source = "../../"
 
-  resource_group_name                 = azurerm_resource_group.test.name
-  location                            = azurerm_resource_group.test.location
-  function_app_name                   = "fpn-to-stop-${random_pet.suffix.id}"
-  service_plan_name                   = "spn-to-stop-${random_pet.suffix.id}"
-  storage_account_name                = azurerm_storage_account.test.name
-  storage_account_resource_group_name = azurerm_storage_account.test.resource_group_name
-  scheduler_action                    = "stop"
-  scheduler_ncrontab_expression       = "0 22 * * *"
-  virtual_machine_schedule            = "true"
+  resource_group_name           = azurerm_resource_group.test.name
+  location                      = azurerm_resource_group.test.location
+  function_app_name             = "fpn-to-stop-${random_pet.suffix.id}"
+  service_plan_name             = "spn-to-stop-${random_pet.suffix.id}"
+  storage_account_name          = "santostop${random_id.suffix.hex}"
+  scheduler_action              = "stop"
+  scheduler_ncrontab_expression = "0 22 * * *"
+  virtual_machine_schedule      = "true"
   custom_app_settings = {
     "test" = random_pet.suffix.id
   }
@@ -40,15 +31,14 @@ module "stop_virtual_machines" {
 module "start_virtual_machines" {
   source = "../../"
 
-  resource_group_name                 = azurerm_resource_group.test.name
-  location                            = azurerm_resource_group.test.location
-  function_app_name                   = "fpn-to-start-${random_pet.suffix.id}"
-  service_plan_name                   = "spn-to-start-${random_pet.suffix.id}"
-  storage_account_name                = azurerm_storage_account.test.name
-  storage_account_resource_group_name = azurerm_storage_account.test.resource_group_name
-  scheduler_action                    = "start"
-  scheduler_ncrontab_expression       = "0 7 * * *"
-  virtual_machine_schedule            = "true"
+  resource_group_name           = azurerm_resource_group.test.name
+  location                      = azurerm_resource_group.test.location
+  function_app_name             = "fpn-to-start-${random_pet.suffix.id}"
+  service_plan_name             = "spn-to-start-${random_pet.suffix.id}"
+  storage_account_name          = "santostart${random_id.suffix.hex}"
+  scheduler_action              = "start"
+  scheduler_ncrontab_expression = "0 7 * * *"
+  virtual_machine_schedule      = "true"
   custom_app_settings = {
     "test" = random_pet.suffix.id
   }
