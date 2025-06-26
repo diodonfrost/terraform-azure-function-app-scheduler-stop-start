7093485a407583b43622cf48cea19552feb23330
diodonfrost <diodon.frost@diodonfrost.me>
2025-06-27 00:51:28 +0200

    refactor(examples): separate network resources into dedicated files
    Move azurerm_virtual_network and azurerm_subnet resources from main.tf to network.tf
    files in all example directories for better organization and maintainability.

---
+++ examples/simple/network.tf
@@ -0,0 +1,13 @@
+resource "azurerm_virtual_network" "test" {
+  name                = "test-${random_pet.suffix.id}"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+}
+
+resource "azurerm_subnet" "test" {
+  name                 = "test-${random_pet.suffix.id}"
+  resource_group_name  = azurerm_resource_group.test.name
+  virtual_network_name = azurerm_virtual_network.test.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
